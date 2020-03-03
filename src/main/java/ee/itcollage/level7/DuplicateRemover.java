d3bc74dfd4abc646b062a454d2e3047f4b367021
Krzysztof Stasiak <krstas@ttu.ee>
2020-03-03 10:29:05 +0200

    level 7 finished

--- src/main/java/ee/itcollage/level7/DuplicateRemover.java
+++ src/main/java/ee/itcollage/level7/DuplicateRemover.java
@@ -1,11 +1,15 @@
 package ee.itcollage.level7;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class DuplicateRemover {
 
     //todo fix tests
     public static List<Integer> removeDuplicates(List<Integer> numbers){
-        return numbers;
+        HashSet<Integer> result = new HashSet<>(numbers);
+            return new ArrayList<>(result);
     }
 }
