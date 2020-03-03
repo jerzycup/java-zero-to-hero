a7d571f82ccfef188e10b58fdf48f02edb0703d9
Krzysztof Stasiak <krstas@ttu.ee>
2020-03-03 10:37:48 +0200

    level 9 finished

--- src/main/java/ee/itcollage/level9/StringMaker.java
+++ src/main/java/ee/itcollage/level9/StringMaker.java
@@ -9,7 +9,10 @@ public class StringMaker {
      * @param x how many times you want the string
      */
     public static String make(String s, int x) {
-        String chars = "";
+        if(x<0){
+            x=0;
+        }
+        String chars = s.repeat(x);
         return chars;
     }
 
@@ -17,7 +20,13 @@ public class StringMaker {
      * Gives you a text containing of x times of string c plus y times of string c.
      */
     public static String make(String s1, int x1, String s2, int x2) {
-        String chars = "";
+        if(x1<0){
+            x1=0;
+        }
+        if(x2<0){
+            x2=0;
+        }
+        String chars = s1.repeat(x1) + s2.repeat(x2);
         return chars;
     }
 }
